5d5f5148507f8f3420a00a2d9b8c13944af9c5e3
BRosencrans <brandon.rosencrans@gmail.com>
2023-03-13 18:54:21 -0700

    maybe fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', routes);
+app.use('/api', routes);
 
 db.once("open", () => {
     app.listen(PORT, () => {
