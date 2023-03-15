27c3a99c1a88f7cc7a24d546bbcd63e37f220fdd
Clarice Tran <tran.clarice@gmail.com>
2023-03-14 22:47:10 -0700

    Minor fixes to db

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 
-const { getUsers, getSingleUser, loginUser, createUser, updateUser, deleteUser } = require("../../controllers/userController");
+const { getUsers, getSingleUser, loginUser, createUser, updateUser, deleteUser, isValidToken } = require("../../controllers/userController");
 
 router.route("/").get(getUsers);
 router.route("/login").post(loginUser);
 router.route("/signup").post(createUser);
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route("/isValidToken").get(isValidToken);
 
 module.exports = router;
